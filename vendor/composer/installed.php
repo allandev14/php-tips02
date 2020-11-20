a3529f47396adb677f5e3c2b721a15a274fcd5a3
allan sousa <allanportinfo@gmail.com>
2020-11-20 11:45:35 -0300

    new project php-tips02

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'allandev14/phptips01',
+  ),
+  'versions' => 
+  array (
+    'allandev14/phptips01' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'coffeecode/datalayer' => 
+    array (
+      'pretty_version' => '1.1.7',
+      'version' => '1.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a51fa227bba003be11519e7efa8338bc87460f5',
+    ),
+  ),
+);
